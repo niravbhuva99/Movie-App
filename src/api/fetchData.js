5b562554b798186f25f79f142351a4ad63d6a681
nirav bhuva <niravbhuva99@gmail.com>
2024-03-24 12:55:37 +0100

    add

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -1,5 +1,5 @@
 const BASEURL = "https://api.themoviedb.org/3";
-
+// h
 const options = {
   headers: {
     accept: "application/json",
@@ -7,7 +7,6 @@ const options = {
   },
 };
 const fetchData = async (url = "") => {
-  console.log(BASEURL + url);
   const res = await fetch(BASEURL + url, options);
   const data = await res.json();
 
